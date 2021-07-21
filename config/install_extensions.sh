2243308d7b8d4f1fb8d7db5b72f0ac9f4af2c338
csprock <csprock@gmail.com>
2021-07-20 23:19:05 -0700

    added optional environment variable INSTALL_EXTENSIONS and conditional logic to init script

--- config/install_extensions.sh
+++ config/install_extensions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "--- Initializing VS Code extensions ---"
-code-server --extensions-dir /config/extensions --install-extension Ikuyadeu.r
-code-server --extensions-dir /config/extensions --install-extension REditorSupport.r-lsp
-code-server --extensions-dir /config/extensions --install-extension GrapeCity.gc-excelviewer
-
+if [-z $INSTALL_EXTENSIONS]; then
+    echo "--- Initializing VS Code extensions ---"
+    code-server --extensions-dir /config/extensions --install-extension Ikuyadeu.r
+    code-server --extensions-dir /config/extensions --install-extension REditorSupport.r-lsp
+    code-server --extensions-dir /config/extensions --install-extension GrapeCity.gc-excelviewer
+else:
+    echo "--- Skipping extension install ---"
+fi
