1861ad38b31cd53bb22ca64c6853936b15b21296
csprock <csprock@gmail.com>
2021-07-22 11:10:41 -0700

    fixed bug in install_extensions.sh

--- config/install_extensions.sh
+++ config/install_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [-z $INSTALL_EXTENSIONS]; then
+if [ -n $INSTALL_EXTENSIONS ]; then
     echo "--- Initializing VS Code extensions ---"
     code-server --extensions-dir /config/extensions --install-extension Ikuyadeu.r
     code-server --extensions-dir /config/extensions --install-extension REditorSupport.r-lsp
